356688750e7fe559f27dc6689498fb980f54a791
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-17 22:01:15 +0800

    Update GPlayer.py

--- GPlayer.py
+++ GPlayer.py
@@ -23,214 +23,203 @@ class GPlayer:
 		self.pipelinesexist = []
 		self.pipelines = []
 		self.pipelines_state = []
-		self.cameraformat = []
+		self.cameraformat = get_video_format()
 		
 		GObject.threads_init()
 		Gst.init(None)
 
-		pipelinesexist, pipelines, cameraformat = createPipelines()
-		for i in pipelines:
-			pipelines_state.append(False)
+		createPipelines()
+		
 
 
-		server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		server.bind((SERVER_IP, IN_PORT))
-		server.setblocking(0)
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.server.bind((self.SERVER_IP, self.IN_PORT))
+		self.server.setblocking(0)
 
-		print(f'server started at {IN_PORT}')
-		print(f'send message to {P_CLIENT_IP}, Port: {IN_PORT}')
+		print(f'server started at {self.IN_PORT}')
+		print(f'send message to {self.P_CLIENT_IP}, Port: {self.IN_PORT}')
+		
+		self.thread_terminate = False
+		self.lock = threading.Lock()
 
-		lock = threading.Lock()
+		self.thread_cli = threading.Thread(target=self.aliveLoop)
+		self.thread_ser = threading.Thread(target=self.listenLoop)
 
-		thread_cli = threading.Thread(target=aliveSignal)
-		thread_ser = threading.Thread(target=listenLoop, args=(server,))
-
-		thread_cli.Client_ip = "192.168.0.1"
-
-		thread_cli.start()
-		thread_ser.start()
+		self.thread_cli.start()
+		self.thread_ser.start()
 		
 	def __del__(self):
-		thread_ser.do_run = False
-		thread_cli.do_run = False
-		thread_cli.join()
-		thread_ser.join()
+		self.thread_terminate = True
+		self.thread_cli.join()
+		self.thread_ser.join()
 
-	def aliveSignal():
+	def aliveLoop(self):
 		print('client started...')
-		t = threading.current_thread()
-		while getattr(t, "do_run", True):
+		run = True
+		while run:
+			if self.thread_terminate is True:
+				break
 			beat = 'alive ' + self.BOAT_NAME
+			# Send primary heartbeat every 0.5s
 			try:
-				client.sendto(beat.encode(),(self.P_CLIENT_IP,self.OUT_PORT))
+				self.client.sendto(beat.encode(),(self.P_CLIENT_IP,self.OUT_PORT))
 				time.sleep(0.5)
 				print(f"Primary send to: {self.P_CLIENT_IP}:{self.OUT_PORT}")
 			except:
 				print(f"Primary unreached: {self.P_CLIENT_IP}:{self.OUT_PORT}")
+			# Send secondary heartbeat every 0.5s
 			try:
-				client.sendto(beat.encode(),(self.S_CLIENT_IP, self.OUT_PORT))
+				self.lient.sendto(beat.encode(),(self.S_CLIENT_IP, self.OUT_PORT))
 				time.sleep(0.5)
 				print(f"Secondarysend to: {self.S_CLIENT_IP}:{self.OUT_PORT}")
 			except:
 				print(f"Secondary unreached: {self.S_CLIENT_IP}:{self.OUT_PORT}")
 
-def createPipelines():
-	_pipelines = []
-	_pipelinesexist = []
-	camera_format = get_video_format()
-	for i in camera_format:
-		j = int(i.split()[0][5]);
-		if(j not in _pipelinesexist):
-			pipeline = Gst.Pipeline()
-			_pipelines.append(pipeline)
-			_pipelinesexist.append(j)
-	print(_pipelinesexist)
-	return _pipelinesexist, _pipelines, camera_format
+	def createPipelines():
+		for i in self.camera_format:
+			j = int(i.split()[0][5]);
+			if(j not in self.pipelinesexist):
+				pipeline = Gst.Pipeline()
+				self.pipelines.append(pipeline)
+				self.pipelinesexist.append(j)
+		for i in self.pipelines:
+			self.pipelines_state.append(False)
+		print(self.pipelinesexist)
 	
 
 #get video format from existing camera devices
-def get_video_format():	
-	camera_format = []
-	
-	#Check camera device
-	for i in range(0,10):
+	def get_video_format(self):	
+		#Check camera device
+		for i in range(0,10):
+				try:
+					cmd = "v4l2-ctl -d /dev/video{} --list-formats-ext".format(i)
+					returned_value = subprocess.check_output(cmd,shell=True).replace(b'\t',b'').decode("utf-8")  # returns the exit code in unix
+				except:
+					continue
+				line_list = returned_value.splitlines()
+				new_line_list = list()
+				for j in line_list:
+					if len(j.split()) == 0:
+						continue
+					elif j.split()[0][0] =='[':
+						form = j.split()[1][1:-1]
+					elif j.split()[0] =='Size:':
+						size = j.split()[2]
+						width, height = size.split('x')
+					elif j.split()[0] == 'Interval:':
+						self.camera_format.append('video{} {} width={} height={} framerate={}'.format(i,form, width, height , j.split()[3][1:].split('.')[0]))
+						print('video{} {} width={} height={} framerate={}'.format(i,form, width, height , j.split()[3][1:].split('.')[0]))
+		return self.camera_format
+
+	def listenLoop(self):
+		print('server started...')
+		run = True
+		while run:
+			if self.thread_terminate is True:
+				break
 			try:
-				cmd = "v4l2-ctl -d /dev/video{} --list-formats-ext".format(i)
-				returned_value = subprocess.check_output(cmd,shell=True).replace(b'\t',b'').decode("utf-8")  # returns the exit code in unix
+				indata, addr = self.server.recvfrom(1024)
+				indata = indata.decode()
 			except:
 				continue
-			line_list = returned_value.splitlines()
-			new_line_list = list()
-			for j in line_list:
-				if len(j.split()) == 0:
-					continue
-				elif j.split()[0][0] =='[':
-					form = j.split()[1][1:-1]
-				elif j.split()[0] =='Size:':
-					size = j.split()[2]
-					width, height = size.split('x')
-				elif j.split()[0] == 'Interval:':
-					camera_format.append('video{} {} width={} height={} framerate={}'.format(i,form, width, height , j.split()[3][1:].split('.')[0]))
-					print('video{} {} width={} height={} framerate={}'.format(i,form, width, height , j.split()[3][1:].split('.')[0]))
-	return camera_format
 
-def listenLoop(ser):
-	
-	
-	print('server started...')
-	t = threading.current_thread()
-	while getattr(t, "do_run", True):
-		try:
-			indata, addr = server.recvfrom(1024)
-			indata = indata.decode()
-		except:
-			continue
+			#print(f'message from: {str(addr)}, data: {indata}')
+			header = indata.split()[0]
 
-		#print(f'message from: {str(addr)}, data: {indata}')
-		header = indata.split()[0]
-
-		if header == 'HB':
-			#thread_cli.Client_ip = indata.split()[1]
-			
-			BOAT_NAME = indata.split()[2]
-			primary = indata.split()[3]
-			if primary == 'P':
-				self.P_CLIENT_IP = indata.split()[1]
-			else:
-				self.S_CLIENT_IP = indata.split()[1]
-
-		if header == 'qformat':
-			print("format")
-			msg = 'format '+BOAT_NAME+'\n'+'\n'.join(cameraformat)
-
-			client.sendto(msg.encode(),(self.P_CLIENT_IP,self.OUT_PORT))
-			client.sendto(msg.encode(),(self.S_CLIENT_IP,self.OUT_PORT))
-		if header == 'cmd':
-			print("cmd")
-			print(indata)
-			cformat = indata.split()[1:6]
-			
-			print(cformat)
-			encoder, mid, quality, ip, port = indata.split()[6:]
-			print(quality, ip, port)
-
-			if(' '.join(cformat) not in cameraformat):
-				print('format error')
-			else:
-				gstring = 'v4l2src device=/dev/'+cformat[0]
-				if cformat[1] == 'YUYV':
-					cformat[1] = 'YUY2'
-					gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={},framerate={}/1 ! '.format(cformat[1],cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
-					if mid != 'nan':
-						gstring += (mid+' ! ')
-					if encoder == 'h264':
-						gstring +=' videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
-					else:
-						gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
-				elif cformat[1] == 'MJPG':
-					gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
-					if mid != 'nan':
-						gstring += (mid+' ! ')
-					if encoder == 'h264':
-						gstring +=' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
-					else:
-						gstring +='jpegparse ! jpegdec ! jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
-					
-				elif cformat[1] == 'GREY':
-					gstring += ' num-buffers=-1 ! video/x-raw,format=GRAY8 ! videoscale ! videoconvert ! video/x-raw, format=YUY2, width=640,height=480 ! '
-					if mid != 'nan':
-						gstring += (mid+' ! ')
-					if encoder == 'h264':
-						gstring +='videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
-					
-					else:
-						gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
-				#elif cformat[1] == 'MJPG':
-                                 #       gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
-                                  #      if encoder == 'h264':
-                                   #             gstring += ' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
-					#else:
-					#	gstring +='jpegparse ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+			if header == 'HB':
+				BOAT_NAME = indata.split()[2]
+				primary = indata.split()[3]
+				if primary == 'P':
+					self.P_CLIENT_IP = indata.split()[1]
 				else:
-					if cformat[1] == 'RGBP':
-						cformat[1] = 'RGB16'
-					elif cformat[1] == 'BGR8':
-						cformat[1] = 'BGR'
-					elif cformat[1] == 'Y1':
-						cformat[1] = 'UYVY'
-					gstring += ' num-buffers=-1 ! video/x-raw,format={}! videoscale ! videoconvert ! video/x-raw, format=YUY2, width=640,height=480 ! '.format(cformat[1])
-					if mid != 'nan':
-						gstring += (mid+' ! ')
-					if encoder == 'h264':
-						gstring +='videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
-						
+					self.S_CLIENT_IP = indata.split()[1]
+
+			if header == 'qformat':
+				print("format")
+				msg = 'format '+BOAT_NAME+'\n'+'\n'.join(cameraformat)
+
+				client.sendto(msg.encode(),(self.P_CLIENT_IP,self.OUT_PORT))
+				client.sendto(msg.encode(),(self.S_CLIENT_IP,self.OUT_PORT))
+			if header == 'cmd':
+				print("cmd")
+				print(indata)
+				cformat = indata.split()[1:6]
+
+				print(cformat)
+				encoder, mid, quality, ip, port = indata.split()[6:]
+				print(quality, ip, port)
+
+				if(' '.join(cformat) not in cameraformat):
+					print('format error')
+				else:
+					gstring = 'v4l2src device=/dev/'+cformat[0]
+					if cformat[1] == 'YUYV':
+						cformat[1] = 'YUY2'
+						gstring += ' num-buffers=-1 ! video/x-raw,format={},width={},height={},framerate={}/1 ! '.format(cformat[1],cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+						if mid != 'nan':
+							gstring += (mid+' ! ')
+						if encoder == 'h264':
+							gstring +=' videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
+						else:
+							gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+					elif cformat[1] == 'MJPG':
+						gstring += ' num-buffers=-1 ! image/jpeg,width={},height={},framerate={}/1 ! '.format(cformat[2].split('=')[1],cformat[3].split('=')[1],cformat[4].split('=')[1])
+						if mid != 'nan':
+							gstring += (mid+' ! ')
+						if encoder == 'h264':
+							gstring +=' jpegparse ! jpegdec ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)	
+						else:
+							gstring +='jpegparse ! jpegdec ! jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+
+					elif cformat[1] == 'GREY':
+						gstring += ' num-buffers=-1 ! video/x-raw,format=GRAY8 ! videoscale ! videoconvert ! video/x-raw, format=YUY2, width=640,height=480 ! '
+						if mid != 'nan':
+							gstring += (mid+' ! ')
+						if encoder == 'h264':
+							gstring +='videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
+
+						else:
+							gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 					else:
-						gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
+						if cformat[1] == 'RGBP':
+							cformat[1] = 'RGB16'
+						elif cformat[1] == 'BGR8':
+							cformat[1] = 'BGR'
+						elif cformat[1] == 'Y1':
+							cformat[1] = 'UYVY'
+						gstring += ' num-buffers=-1 ! video/x-raw,format={}! videoscale ! videoconvert ! video/x-raw, format=YUY2, width=640,height=480 ! '.format(cformat[1])
+						if mid != 'nan':
+							gstring += (mid+' ! ')
+						if encoder == 'h264':
+							gstring +='videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host={} port={}'.format(ip, port)
 
-				
-				print(gstring)
-				print(cformat[1])
-				print(cformat[1][5:])
-				videoindex = pipelinesexist.index(int(cformat[0][5:]))
-				
+						else:
+							gstring +='jpegenc quality=30 ! rtpjpegpay ! udpsink host={} port={}'.format(ip, port)
 
-				if pipelines_state[videoindex] == True:
+
+					print(gstring)
+					print(cformat[1])
+					print(cformat[1][5:])
+					videoindex = pipelinesexist.index(int(cformat[0][5:]))
+
+
+					if pipelines_state[videoindex] == True:
+						pipelines[videoindex].set_state(Gst.State.NULL)
+						pipelines[videoindex] = Gst.parse_launch(gstring)
+						pipelines[videoindex].set_state(Gst.State.PLAYING)
+
+					else:
+						pipelines[videoindex] = Gst.parse_launch(gstring)
+						pipelines[videoindex].set_state(Gst.State.PLAYING)
+						pipelines_state[videoindex] = True
+			if header == 'quit':
+				video = int(indata.split()[1][5:])
+				if video in pipelinesexist:
+					videoindex = pipelinesexist.index(video)
 					pipelines[videoindex].set_state(Gst.State.NULL)
-					pipelines[videoindex] = Gst.parse_launch(gstring)
-					pipelines[videoindex].set_state(Gst.State.PLAYING)
-
-				else:
-					pipelines[videoindex] = Gst.parse_launch(gstring)
-					pipelines[videoindex].set_state(Gst.State.PLAYING)
-					pipelines_state[videoindex] = True
-		if header == 'quit':
-			video = int(indata.split()[1][5:])
-			if video in pipelinesexist:
-				videoindex = pipelinesexist.index(video)
-				pipelines[videoindex].set_state(Gst.State.NULL)
-				pipelines_state[videoindex] = False
-				print("quit : video"+str(video))
+					pipelines_state[videoindex] = False
+					print("quit : video"+str(video))
 
 
 
