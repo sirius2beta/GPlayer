a0a1d8aeaa01147c153df02eab9c696cb29e9c40
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-23 12:49:41 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,8 +7,8 @@ def on_msg(topic, message):
   if topic == 'testcmd':
     print(f'{topic} : {message}')
 
-gplayer = GPlayer.GPlayer()
-gplayer.on_msg = on_msg
+#gplayer = GPlayer.GPlayer()
+#gplayer.on_msg = on_msg
 
 cap_send = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=20/1 ! videoscale ! videoconvert ! appsink drop=1', cv2.CAP_GSTREAMER)
 #out_send = cv2.VideoWriter('gst-launch-1.0 appsrc ! videoconvert ! omxh264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=100.117.209.85 port=5200',cv2.CAP_GSTREAMER,0, 20, (320,240), True)
