9f81582b798fe804228e646c60308f5db9704bc0
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-23 10:58:09 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,24 +11,18 @@ gplayer = GPlayer.GPlayer()
 gplayer.on_msg = on_msg
 
 cap_send = cv2.VideoCapture('videotestsrc ! video/x-raw,framerate=20/1 ! videoscale ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-    out_send = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000',cv2.CAP_GSTREAMER,0, 20, (320,240), True)
+out_send = cv2.VideoWriter('appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! rtph264pay ! udpsink host=127.0.0.1 port=5000',cv2.CAP_GSTREAMER,0, 20, (320,240), True)
+if not cap_send.isOpened() or not out_send.isOpened():
+  print('VideoCapture or VideoWriter not opened')
+  exit(0)
 
-    if not cap_send.isOpened() or not out_send.isOpened():
-        print('VideoCapture or VideoWriter not opened')
-        exit(0)
-
-    while True:
-        ret,frame = cap_send.read()
-
-        if not ret:
-            print('empty frame')
-            break
-
-        out_send.write(frame)
-
-        cv2.imshow('send', frame)
-        if cv2.waitKey(1)&0xFF == ord('q'):
-            break
-
-    cap_send.release()
-    out_send.release()
+while True:
+  ret,frame = cap_send.read()
+  if not ret:
+    print('empty frame')
+    break
+    out_send.write(frame)
+    if cv2.waitKey(1)&0xFF == ord('q'):
+      break
+cap_send.release()
+out_send.release()
