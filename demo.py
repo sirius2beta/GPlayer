df86e1f280b4774adf6214efb6257ca5cd6a9ca1
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-23 13:37:32 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -22,7 +22,7 @@ while True:
     print('empty frame')
     break
     cv2.imshow('send', frame)
-    #out_send.write(frame)
+    out_send.write(frame)
     if cv2.waitKey(1)&0xFF == ord('q'):
       break
 cap_send.release()
