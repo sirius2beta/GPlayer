1b162b2df4ed6f7eba0c0d2ee94fbe1cb8363b7b
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-25 17:07:53 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,20 +4,30 @@ from ultralytics.yolo.utils.plotting import Annotator
 
 
 model = YOLO('yolov8n.pt')
-x_line = 100
+cap = cv2.VideoCapture(0)
 
-img = cv2.imread('zidane.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-results = model.predict(img, conf=0.5, classes=0)
-annotator = Annotator(img)
 
-for r in results:
-    for box in r.boxes:
-        b = box.xyxy[0]
-        if b[1] > x_line:
-            c = box.cls
-            annotator.box_label(b, f"{r.names[int(c)]} {float(box.conf):.2}")
+if not cap.isOpened():
+    print('cap not opened')
+    exit(0)
+while True:
+    ret, frame = cap.read()
+    if ret:
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = model.predict(img, conf=0.5, classes=0)
+        annotator = Annotator(img)
 
-img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-cv2.line(img, (0, x_line), (img.shape[1] - 1, x_line), (255, 0, 0), 2)
-cv2.imwrite("YOLO.jpg", img)
+        for r in results:
+            for box in r.boxes:
+                b = box.xyxy[0]
+                annotator.box_label(b, f"{r.names[int(c)]} {float(box.conf):.2}")
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imshow(“windows”, img)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
