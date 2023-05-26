c97d60677e623e4c70db34fa9da350e69409a0ab
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-27 00:59:34 +0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -252,6 +252,12 @@ cap_send = cv2.VideoCapture(video_pipeline, cv2.CAP_GSTREAMER)
 w = cap_send.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cap_send.get(cv2.CAP_PROP_FRAME_HEIGHT)
 fps = cap_send.get(cv2.CAP_PROP_FPS)
+out_send = cv2.VideoWriter('appsrc ! videoconvert ! video/x-raw,format=I420 ! nvvideoconvert ! video/x-raw(memory:NVMM) ! nvv4l2h264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=100.117.209.85 port=5201'\
+         ,cv2.CAP_GSTREAMER\
+         ,0\
+         , 30\
+         , (640, 480)\
+         , True)
 
 
 if not cap_send.isOpened():
