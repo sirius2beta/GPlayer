0e45ea9cb6b7bb5df1770924c8d89c5c85ea528b
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-26 17:22:01 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,12 +16,12 @@ cap_send = cv2.VideoCapture(video_pipeline, cv2.CAP_GSTREAMER)
 w = cap_send.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cap_send.get(cv2.CAP_PROP_FRAME_HEIGHT)
 fps = cap_send.get(cv2.CAP_PROP_FPS)
-out_send = cv2.VideoWriter('appsrc ! nvvidconv ! nvv4l2h264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=100.117.209.85 port=5240'\
-                           ,cv2.CAP_GSTREAMER\
-                           ,0\
-                           , fps\
-                           , (int(w), int(h))\
-                           , True)
+out_send = cv2.VideoWriter('appsrc !  nvvidconv ! nvv4l2h264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=100.117.209.85 port=5240'\
+         ,cv2.CAP_GSTREAMER\
+         ,0\
+         , 30\
+         , (1920, 1080)\
+         , True)
 if not cap_send.isOpened() or not out_send.isOpened():
   print('VideoCapture or VideoWriter not opened')
   exit(0)
