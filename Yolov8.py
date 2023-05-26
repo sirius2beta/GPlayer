f7efdfa1e95d351da6f94db9007719fc524ed867
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-26 16:54:31 +0800

    Update Yolov8.py

--- Yolov8.py
+++ Yolov8.py
@@ -209,13 +209,15 @@ CLASSES = ('person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
            'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
            'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
            'scissors', 'teddy bear', 'hair drier', 'toothbrush')      
-      
-out_send = cv2.VideoWriter('appsrc !  nvvidconv ! nvv4l2h264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=127.0.0.1 port=5240'\
-                           ,cv2.CAP_GSTREAMER\
-                           ,0\
-                           , 30\
-                           , (1920, 1080)\
-                           , True)
+class VR:
+    def __init__(self):
+        out_send = cv2.VideoWriter('appsrc !  nvvidconv ! nvv4l2h264enc ! rtph264pay pt=96 config-interval=1 ! udpsink host=127.0.0.1 port=5240'\
+                                   ,cv2.CAP_GSTREAMER\
+                                   ,0\
+                                   , 30\
+                                   , (1920, 1080)\
+                                   , True)
+vr = VR()
 enggine = TRTEngine('yolov8s.engine')
 H, W = enggine.inp_info[0].shape[-2:]
 
