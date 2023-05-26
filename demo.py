e8bcc03b2424282d553bcf79cb528c1ba5fdc5ae
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-05-26 18:15:48 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,16 +2,7 @@ import GPlayer
 import time
 import cv2
 
-
-def on_msg(topic, message):
-  print('onMsg callback;')
-  if topic == 'testcmd':
-    print(f'{topic} : {message}')
-
-#gplayer = GPlayer.GPlayer()
-#gplayer.on_msg = on_msg
-#video_pipeline = 'v4l2src device=/dev/video0 !  video/x-raw, format=YUY2, width=640, height=480, framerate=30/1 ! videoconvert ! appsink'
-video_pipeline = 'videotestsrc ! videoconvert ! appsink'
+video_pipeline = 'video/x-raw, format=BGR ! videoconvert ! appsink'
 cap_send = cv2.VideoCapture(video_pipeline, cv2.CAP_GSTREAMER)
 w = cap_send.get(cv2.CAP_PROP_FRAME_WIDTH)
 h = cap_send.get(cv2.CAP_PROP_FRAME_HEIGHT)
